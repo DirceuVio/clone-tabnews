b48f71cda7acee26373eaf425def2fea44f292ff
Dirceu Silva <ddasilvajunior@vio.com>
2024-03-02 17:10:43 -0300

    adds `env` and `database.js` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS result;");
+  console.log(result.rows);
   response.status(200).json({ message: "Eu amo minha esposa" });
 }
 
