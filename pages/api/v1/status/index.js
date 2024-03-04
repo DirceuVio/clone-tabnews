aa21ba5eff3bbc9b61c7d55450cbf15b8427466f
Dirceu Silva <ddasilvajunior@vio.com>
2024-03-04 21:04:43 +0100

    finish `\status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS result;");
-  console.log(result.rows);
-  response.status(200).json({ message: "Eu amo minha esposa" });
-}
+  const updateAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  const dbStatus = await database.query({
+    text: `SELECT 
+      current_setting('server_version') AS server_version,
+      current_setting('max_connections')::int AS max_connections,
+      SUM(CASE WHEN datname = $1 THEN 1 ELSE 0 END)::int AS current_connections
+    FROM 
+        pg_stat_activity`,
+    values: [databaseName],
+  });
 
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbStatus.rows[0].server_version,
+        max_connections: dbStatus.rows[0].max_connections,
+        current_connections: dbStatus.rows[0].current_connections,
+      },
+    },
+  });
+}
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    expect.any(Number),
+  );
+
+  expect(responseBody.dependencies.database.current_connections).toBeDefined();
+  expect(responseBody.dependencies.database.current_connections).toEqual(1);
 });
