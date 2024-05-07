d86c68f9b92567e5f5e367c116f8f1aec526ebb4
Dirceu Silva <ddasilvajunior@vio.com>
2024-05-07 20:44:04 -0300

    Add  endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,36 @@
+import mingrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method == "GET") {
+    const pendingMigrations = await mingrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method == "POST") {
+    const migratedMigrations = await mingrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).json({ message: "Method not allowed" });
+}
