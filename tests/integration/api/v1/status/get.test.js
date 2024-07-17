c5effd11a7a06d4aa78f25448455781e18fcabf6
Dirceu Silva <ddasilvajunior@vio.com>
2024-07-17 16:38:38 -0300

    Ads automated tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -15,9 +15,7 @@ test("GET to api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(responseBody.dependencies.database.version).toBeDefined();
-  expect(
-    parseFloat(responseBody.dependencies.database.version),
-  ).toBeGreaterThanOrEqual(16.0);
+  expect(parseFloat(responseBody.dependencies.database.version)).toEqual(16.0);
 
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
   expect(responseBody.dependencies.database.max_connections).toEqual(
